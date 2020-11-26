01da9ded6a374c476b2241209e7f05c644df6890
Mykola Steliuk <msteliuk@rubiconproject.com>
2020-11-26 17:06:28 +0200

    moved

--- proposals/Gatekeeper-POC/GatekeeperChromeExt/background.js
+++ proposals/Gatekeeper-POC/GatekeeperChromeExt/background.js
@@ -1,6 +1,6 @@
-var SESSIONID_URL = 'http://ec2-34-209-240-19.us-west-2.compute.amazonaws.com:8000/api/v1/cohort/' ;
+var SESSIONID_URL = 'http://ec2-34-209-240-19.us-west-2.compute.amazonaws.com:8000/api/v1/cohort/';
 
-const interval = setInterval(function() {
+const interval = setInterval(function () {
     console.log("The session id is reset ");
     resettingSessionId();
 }, 18000000);
@@ -12,23 +12,57 @@ function resettingSessionId() {
     });
 }
 
-function setSessionIdStorage(sid){
-    console.log(" Setting session id" +sid);
+function setSessionIdStorage(sid) {
+    console.log(" Setting session id" + sid);
     var sessionIdData = {}
     sessionIdData.sid = sid
     chrome.storage.sync.set({"GatekeeperId": sessionIdData}, function () {
-        console.log(" Gatekeeper data: "+ sessionIdData);
+        console.log(" Gatekeeper data: " + sessionIdData);
     });
 }
 
-function setCohortStorage(cohort, score){
-    console.log(" Setting cohort" +cohort);
-    var cohortData = {}
-    cohortData.cohort = cohort
-    cohortData.score = score
+function setCohortStorage(cohort, score) {
+    console.log(" Setting cohort" + cohort);
+    var cohortData = {};
+    cohortData.cohort = cohort;
+    cohortData.score = score;
+
     chrome.storage.sync.set({"Cohort": cohortData}, function () {
-        console.log(" Cohort to storage: "+ cohortData);
+        console.log(" Cohort to storage: " + cohortData);
     });
+}
+
+function setUrlData(url) {
+    console.log(" Setting url " + url);
+
+    if (!url) {
+        console.log("Invalid data %s", url);
+        return;
+    }
+
+    chrome.storage.sync.get([url], function (res) {
+        if (url in res) {
+
+            res[url]["date"].push(new Date().getTime());
+            res[url]["amount"] = res[url]["date"].length;
+
+            chrome.storage.sync.set(res, function () {
+                console.log("Old  Url set: " + url + " value: " + res[url]["amount"]);
+            });
+        } else {
+            var data = {};
+            var date = new Date().getTime();
+
+            data[url] = {
+                amount: 1,
+                date: [date]
+            };
+
+            chrome.storage.sync.set(data, function () {
+                console.log("New set: " + url + " value: " + JSON.stringify(data[url]));
+            });
+        }
+    })
 }
 
 function makeIdRequest(callback) {
@@ -43,14 +77,15 @@ function makeIdRequest(callback) {
 
 function postRequest(uri, sid) {
     console.log(" uri " + uri);
-    if(uri.includes("?")){
+    if (uri.includes("?")) {
         uri = uri.substring(0, uri.indexOf('?'));
     }
     console.log("Posted uri" + uri);
 
-    if(uri.length > 0) {
-        var sessionid = ""
-        if(sid!= undefined && !isEmptyObject(sid)){
+    if (uri.length > 0) {
+        var sessionid = "";
+
+        if (sid !== undefined && !isEmptyObject(sid)) {
             sessionid = sid.GatekeeperId.sid;
         }
 
@@ -66,28 +101,30 @@ function postRequest(uri, sid) {
                 console.log(" Response " + json)
                 setSessionIdStorage(json.session_id);
                 setCohortStorage(json.cohort, json.c_score);
-
             }
         }
     }
 }
 
-function ValidateSessionAndPost(uri){
+function ValidateSessionAndPost(uri) {
 
-    if(!isValidURL(uri)){
+    if (!isValidURL(uri)) {
         console.log("Invalid uri" + uri);
         return;
     }
     chrome.storage.sync.get('GatekeeperId', function (obj) {
         console.log('GatekeeperId', obj.GatekeeperId);
-            postRequest(uri,obj);
+        postRequest(uri, obj);
     });
 }
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    console.log('Tab Updated')
+    console.log('Tab Updated');
+
     if (changeInfo.url) {
         ValidateSessionAndPost(changeInfo.url);
+        setUrlData(changeInfo.url);
+        getLastDayDataAboutUrl(changeInfo.url);
     }
 
     if (changeInfo.status === 'loading') {
@@ -95,23 +132,28 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
         injectContentScript(tab);
     }
 
+
 });
 
 chrome.tabs.onCreated.addListener(function (tabId) {
 
     chrome.tabs.getSelected(null, function (tab) {
         //get current tab without any selectors
-        console.log(" Tab created with url "+ tab.url);
+        console.log(" Tab created with url " + tab.url);
         ValidateSessionAndPost(tab.url);
     });
 
 });
 
-chrome.tabs.onActivated.addListener(function(info) {
-    var tab = chrome.tabs.get(info.tabId, function(tab) {
+chrome.tabs.onActivated.addListener(function (info) {
+    var tab = chrome.tabs.get(info.tabId, function (tab) {
         //get current tab without any selectors
-        console.log(" Tab activated with url "+ tab.url);
+        console.log(" Tab activated with url " + tab.url);
+        var url = domainFromUrl(tab.url)
+        console.log("url:" + url);
         ValidateSessionAndPost(tab.url);
+        setUrlData(url);
+        getLastDayDataAboutUrl(tab.url);
     });
 
 });
@@ -119,13 +161,13 @@ chrome.tabs.onActivated.addListener(function(info) {
 function isValidURL(string) {
     var res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
     return (res !== null)
-};
+}
 
 function injectContentScript(tab) {
     console.log('Injecting script');
-    chrome.storage.sync.get(['Cohort'], function(result) {
+    chrome.storage.sync.get(['Cohort'], function (result) {
 
-        if(isEmptyObject(result)){
+        if (isEmptyObject(result)) {
             return;
         }
         const cohort = result.Cohort.cohort;
@@ -148,6 +190,101 @@ function injectContentScript(tab) {
     })
 }
 
-function isEmptyObject(obj){
+function isEmptyObject(obj) {
     return JSON.stringify(obj) === '{}';
+}
+
+function domainFromUrl(url) {
+    var result;
+    var match;
+
+    if (match = url.match(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n\?\=]+)/im)) {
+        result = match[1]
+        if (match = result.match(/^[^\.]+\.(.+\..+)$/)) {
+            result = match[1]
+        }
+    }
+    return result
+}
+
+function getLastDayDataAboutUrl(url) {
+    console.log("GET LAST DATA CALLED");
+    if (!url) {
+        console.log("Invalid url " + url);
+        return;
+    }
+
+    url = domainFromUrl(url);
+
+    chrome.storage.sync.get([url], function (res) {
+        if (!res) {
+            return;
+        }
+        var cuurentDate = new Date();
+        //todo cuurentDate.setHours(cuurentDate.getHours() - 24);
+        cuurentDate.setHours(cuurentDate.getSeconds() - 1);
+        var allDates = res[url]["date"];
+
+        var response = [];
+
+        allDates.forEach(function (item, index) {
+
+            console.log(item - cuurentDate.getTime());
+
+            if (item > cuurentDate.getTime()) {
+                response.push(item);
+            }
+
+        });
+
+        console.log("get data called with %s res: %s", url, response.length);
+    });
+
+}
+
+chrome.runtime.onMessageExternal.addListener(
+    function (request, sender, sendResponse) {
+
+        chrome.storage.sync.get(null,
+            result => {
+                if (!result) {
+                    sendResponse({response: "no data"});
+                    return;
+                }
+
+                console.log("All data retrieved " + JSON.stringify(result));
+                let keys = Object.keys(result);
+                console.log("All keys: " + keys);
+                let response = {};
+                for (let prop in result) {
+                    if (Object.prototype.hasOwnProperty.call(result, prop)) {
+                        response[prop] = result[prop]["amount"];
+                    }
+                }
+
+                sendResponse(response);
+
+            })
+
+    });
+
+function test() {
+    chrome.storage.sync.get(null,
+        result => {
+            let keys = Object.keys(result);
+            console.log("All keys: " + keys);
+            let response = {};
+            for (let prop in result) {
+                if (Object.prototype.hasOwnProperty.call(result, prop)) {
+                    response[prop] = result[prop]["amount"];
+                }
+            }
+
+            console.log("Response to send " + JSON
+                .stringify(response));
+
+            // sendResponse(response);
+
+        })
+
 }
