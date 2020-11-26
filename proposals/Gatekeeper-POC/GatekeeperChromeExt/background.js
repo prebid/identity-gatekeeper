4cbab2b8fcb570cfb891c09e315bea3cdf947e77
Mykola Steliuk <msteliuk@rubiconproject.com>
2020-11-26 17:18:10 +0200

    clean up

--- proposals/Gatekeeper-POC/GatekeeperChromeExt/background.js
+++ proposals/Gatekeeper-POC/GatekeeperChromeExt/background.js
@@ -32,39 +32,6 @@ function setCohortStorage(cohort, score) {
     });
 }
 
-function setUrlData(url) {
-    console.log(" Setting url " + url);
-
-    if (!url) {
-        console.log("Invalid data %s", url);
-        return;
-    }
-
-    chrome.storage.sync.get([url], function (res) {
-        if (url in res) {
-
-            res[url]["date"].push(new Date().getTime());
-            res[url]["amount"] = res[url]["date"].length;
-
-            chrome.storage.sync.set(res, function () {
-                console.log("Old  Url set: " + url + " value: " + res[url]["amount"]);
-            });
-        } else {
-            var data = {};
-            var date = new Date().getTime();
-
-            data[url] = {
-                amount: 1,
-                date: [date]
-            };
-
-            chrome.storage.sync.set(data, function () {
-                console.log("New set: " + url + " value: " + JSON.stringify(data[url]));
-            });
-        }
-    })
-}
-
 function makeIdRequest(callback) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', SESSIONID_URL);
@@ -122,9 +89,7 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     console.log('Tab Updated');
 
     if (changeInfo.url) {
-        ValidateSessionAndPost(changeInfo.url);
         setUrlData(changeInfo.url);
-        getLastDayDataAboutUrl(changeInfo.url);
     }
 
     if (changeInfo.status === 'loading') {
@@ -152,8 +117,6 @@ chrome.tabs.onActivated.addListener(function (info) {
         var url = domainFromUrl(tab.url)
         console.log("url:" + url);
         ValidateSessionAndPost(tab.url);
-        setUrlData(url);
-        getLastDayDataAboutUrl(tab.url);
     });
 
 });
@@ -192,99 +155,4 @@ function injectContentScript(tab) {
 
 function isEmptyObject(obj) {
     return JSON.stringify(obj) === '{}';
-}
-
-function domainFromUrl(url) {
-    var result;
-    var match;
-
-    if (match = url.match(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n\?\=]+)/im)) {
-        result = match[1]
-        if (match = result.match(/^[^\.]+\.(.+\..+)$/)) {
-            result = match[1]
-        }
-    }
-    return result
-}
-
-function getLastDayDataAboutUrl(url) {
-    console.log("GET LAST DATA CALLED");
-    if (!url) {
-        console.log("Invalid url " + url);
-        return;
-    }
-
-    url = domainFromUrl(url);
-
-    chrome.storage.sync.get([url], function (res) {
-        if (!res) {
-            return;
-        }
-        var cuurentDate = new Date();
-        //todo cuurentDate.setHours(cuurentDate.getHours() - 24);
-        cuurentDate.setHours(cuurentDate.getSeconds() - 1);
-        var allDates = res[url]["date"];
-
-        var response = [];
-
-        allDates.forEach(function (item, index) {
-
-            console.log(item - cuurentDate.getTime());
-
-            if (item > cuurentDate.getTime()) {
-                response.push(item);
-            }
-
-        });
-
-        console.log("get data called with %s res: %s", url, response.length);
-    });
-
-}
-
-chrome.runtime.onMessageExternal.addListener(
-    function (request, sender, sendResponse) {
-
-        chrome.storage.sync.get(null,
-            result => {
-                if (!result) {
-                    sendResponse({response: "no data"});
-                    return;
-                }
-
-                console.log("All data retrieved " + JSON.stringify(result));
-                let keys = Object.keys(result);
-                console.log("All keys: " + keys);
-                let response = {};
-                for (let prop in result) {
-                    if (Object.prototype.hasOwnProperty.call(result, prop)) {
-                        response[prop] = result[prop]["amount"];
-                    }
-                }
-
-                sendResponse(response);
-
-            })
-
-    });
-
-function test() {
-    chrome.storage.sync.get(null,
-        result => {
-            let keys = Object.keys(result);
-            console.log("All keys: " + keys);
-            let response = {};
-            for (let prop in result) {
-                if (Object.prototype.hasOwnProperty.call(result, prop)) {
-                    response[prop] = result[prop]["amount"];
-                }
-            }
-
-            console.log("Response to send " + JSON
-                .stringify(response));
-
-            // sendResponse(response);
-
-        })
-
 }
