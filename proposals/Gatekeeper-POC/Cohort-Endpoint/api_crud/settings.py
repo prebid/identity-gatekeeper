ee040225f847cc92619b79f05cb4d915e947e2b6
Yaroslav Boychuk <yboychuk@magnite.com>
2020-12-04 22:05:04 +0200

    aws deployment

--- proposals/Gatekeeper-POC/Cohort-Endpoint/api_crud/settings.py
+++ proposals/Gatekeeper-POC/Cohort-Endpoint/api_crud/settings.py
@@ -26,7 +26,6 @@ DEBUG = True
 
 # ALLOWED_HOSTS = ['ec2-34-209-240-19.us-west-2.compute.amazonaws.com']
 ALLOWED_HOSTS = [
-    "de-cohort-endpoint.magnite.com",
     "de-cohort-endpoint-dev.usw2.fanops.net"
 ]
 
